9a80f86e6d3dee06d801e642b4b91b417dd4d4d5
Luis <heredialuis20@gmail.com>
2024-04-17 18:46:27 +0200

    carrusel terminado

---
+++ JS/hero-image-carrusel.js
@@ -0,0 +1,48 @@
+// var bgCounter = 0;
+// function heroSlideshow(){
+//     var listaImgBg = [
+//         "linear-gradient(to bottom, rgba(255, 255, 255, 0), var(--color-principal)), url('imagenes/hero-image/hero-image1.jpg')",
+//         "linear-gradient(to bottom, rgba(255, 255, 255, 0), var(--color-principal)), url('imagenes/hero-image/hero-image2.jpg')",
+//         "linear-gradient(to bottom, rgba(255, 255, 255, 0), var(--color-principal)), url('imagenes/hero-image/hero-image3.jpg')",
+//         "linear-gradient(to bottom, rgba(255, 255, 255, 0), var(--color-principal)), url('imagenes/hero-image/hero-image4.jpg')"
+//     ];
+
+//     bgCounter++;
+
+//     if(bgCounter == listaImgBg.length){
+//         bgCounter = 0;
+//     };
+
+//     document.querySelector('.banner').style.backgroundImage = listaImgBg[bgCounter];
+// }
+
+var counterNext = 0;
+var counterMain = 0;
+function slideShowAnim(){
+    
+    var listaImgBg = document.getElementsByClassName('fondosHero');
+
+    counterNext++;
+    counterMain = counterNext-1;
+
+    if(counterNext == listaImgBg.length){
+        counterNext = 0;
+    }
+
+    if(counterMain < 0){
+        counterMain = listaImgBg.length--;
+    }
+    for(var i = 0; i < listaImgBg.length; i++){
+        listaImgBg[i].classList.remove('nextSlide');
+        listaImgBg[i].classList.remove('mainSlide');
+        listaImgBg[i].classList.remove('hiddenSlide');
+
+        if(i == counterNext){
+            listaImgBg[i].classList.add('nextSlide');
+        }else if(i == counterMain){
+            listaImgBg[i].classList.add('mainSlide');
+        }else{
+            listaImgBg[i].classList.add('hiddenSlide');
+        }
+    }
+}
