ddf3b163e2d48dda3d4797b9076af3bf57983b33
Luis <heredialuis20@gmail.com>
2024-04-17 19:56:19 +0200

    centrado el footer

--- JS/hero-image-carrusel.js
+++ JS/hero-image-carrusel.js
@@ -16,33 +16,33 @@
 //     document.querySelector('.banner').style.backgroundImage = listaImgBg[bgCounter];
 // }
 
-var counterNext = 0;
-var counterMain = 0;
-function slideShowAnim(){
+// var counterNext = 0;
+// var counterMain = 0;
+// function slideShowAnim(){
     
-    var listaImgBg = document.getElementsByClassName('fondosHero');
+//     var listaImgBg = document.getElementsByClassName('fondosHero');
 
-    counterNext++;
-    counterMain = counterNext-1;
+//     counterNext++;
+//     counterMain = counterNext-1;
 
-    if(counterNext == listaImgBg.length){
-        counterNext = 0;
-    }
+//     if(counterNext == listaImgBg.length){
+//         counterNext = 0;
+//     }
 
-    if(counterMain < 0){
-        counterMain = listaImgBg.length--;
-    }
-    for(var i = 0; i < listaImgBg.length; i++){
-        listaImgBg[i].classList.remove('nextSlide');
-        listaImgBg[i].classList.remove('mainSlide');
-        listaImgBg[i].classList.remove('hiddenSlide');
+//     if(counterMain < 0){
+//         counterMain = listaImgBg.length--;
+//     }
+//     for(var i = 0; i < listaImgBg.length; i++){
+//         listaImgBg[i].classList.remove('nextSlide');
+//         listaImgBg[i].classList.remove('mainSlide');
+//         listaImgBg[i].classList.remove('hiddenSlide');
 
-        if(i == counterNext){
-            listaImgBg[i].classList.add('nextSlide');
-        }else if(i == counterMain){
-            listaImgBg[i].classList.add('mainSlide');
-        }else{
-            listaImgBg[i].classList.add('hiddenSlide');
-        }
-    }
-}
+//         if(i == counterNext){
+//             listaImgBg[i].classList.add('nextSlide');
+//         }else if(i == counterMain){
+//             listaImgBg[i].classList.add('mainSlide');
+//         }else{
+//             listaImgBg[i].classList.add('hiddenSlide');
+//         }
+//     }
+// }
